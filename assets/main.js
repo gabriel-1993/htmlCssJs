7850073ccb6a56c8413d734a09b626f14c96caa2
Gabriel Torrez <110358252+gabriel-1993@users.noreply.github.com>
2022-12-24 00:17:33 -0300

    RepoInteJS

--- assets/main.js
+++
@@ -1,8 +0,0 @@
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
-const saveLocalStorage = (cartList) => {
-  localStorage.setItem("cart", JSON.stringify(cartList));
-};
-
-const init = () => {};
-
-init();
